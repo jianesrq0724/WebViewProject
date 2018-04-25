78b6d551b599b2f22696eb4b0dee3eb7a0d74d7a
ruiqin.shen <jianesrq0724@outlook.com>
2018-04-25 17:58:43 +0800

    增加花瓣网

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # WebViewProject
+# 必应搜索 
+* browerbiying  https://cn.bing.com/
+
+# http://pdf2doc.com/zh/
---
+++ app/src/picture1/java/com/carl/MainActivity.java
@@ -0,0 +1,20 @@
+package com.carl;
+
+/**
+ * @author Carl
+ * @version 1.0
+ * @since 2018/4/25
+ */
+
+public class MainActivity extends MainBaseActivity {
+
+    @Override
+    protected void initData() {
+        mHomeUrl = "http://huaban.com/";
+    }
+
+    @Override
+    public void hideToolBar() {
+        super.hideToolBar();
+    }
+}
